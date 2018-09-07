2feffc1a0a90af4cbf8cd6dd7092249110acb3a9
Benjamin Otto <benjamin.otto@style-systems.de>
2018-09-07 12:42:58 +0200

    updated deps

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   coverageReporters: ['text'],
   collectCoverageFrom: ['src/**/*.ts', '!src/http/exceptions/*.ts'],
   transform: {
-    '^.+\\.ts?$': '<rootDir>/node_modules/ts-jest/preprocessor.js'
+    '^.+\\.ts?$': 'ts-jest'
   },
   testRegex: '(/tests/.*)\\.(ts?|tsx?)$',
   testPathIgnorePatterns: ['/node_modules/', '/example/', '/dist/'],
