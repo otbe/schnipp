cf4645b67cf07802e48f99252ee59c5ab54a9826
Benjamin Otto <benjamin.otto@style-systems.de>
2019-02-18 09:37:49 +0100

    fixed example

--- example/src/guards/RolesGuard.ts
+++ example/src/guards/RolesGuard.ts
@@ -1,8 +1,7 @@
 import { Guard, MetaData } from '../../../dist/http';
-import { DefaultExecutionContext } from '../../../dist/http/utils/ExecutionContext';
 
 export class RolesGuard implements Guard {
-  canActivate(executionContext: DefaultExecutionContext, metaData: MetaData) {
+  canActivate(executionContext: any, metaData: MetaData) {
     if (metaData.roles) {
       return executionContext
         .authorizer!.roles.split(',')
